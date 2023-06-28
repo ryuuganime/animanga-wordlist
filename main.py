4660229b3703999a0a2ee07121140495340c6a52
Natsu <nattadasu@ryuuganime.my.id>
2023-06-29 01:10:49 +0700

    :+1:

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from getopt import getopt, GetoptError
-import os
-import sys
+# from getopt import getopt, GetoptError
+# import os
+# import sys
 
 from src.annict import Annict
 from src.kaize import Kaize
 from src.otakotaku import OtakOtaku
+from src.silveryasha import SilverYasha
+from src.trakt import Trakt
+from src.animeapi import AnimeApi
+from src.myanimelist_jikan import MyAnimeList
+from src.anilist import AniList
 
 
 def main() -> None:
@@ -25,3 +30,31 @@ def main() -> None:
         python3 main.py --env
     """
 
+
+if __name__ == "__main__":
+
+    kz = Kaize()
+    kza, kzm = kz.save_data()
+    kz.close()
+
+    oo = OtakOtaku()
+    ooa = oo.save_otakotaku_data()
+    oo.close()
+
+    sy = SilverYasha()
+    sya = sy.save_silveryasha_data()
+    sy.close()
+
+    tr = Trakt()
+    tra = tr.save_trakt_data()
+    tr.close()
+
+    aa = AnimeApi()
+    aaa = aa.save_animeapi_data()
+    aa.close()
+
+    mal = MyAnimeList()
+    mala, malm, malpro, malmg = mal.fetch_data_all()
+
+    al = AniList()
+    ala, alm = al.save_anilist_data()
